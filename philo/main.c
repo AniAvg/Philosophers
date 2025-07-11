b7cf8651ab7888b08ad1a893795f5b330f36194f
Ani Avagyan <anavagya@c2r11s1.42yerevan.am>
2025-07-11 18:38:05 +0400

    second commit

--- philo/main.c
+++ philo/main.c
@@ -6,17 +6,45 @@
 /*   By: anavagya <anavgya@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 16:34:54 by anavagya          #+#    #+#             */
-/*   Updated: 2025/07/01 17:38:19 by anavagya         ###   ########.fr       */
+/*   Updated: 2025/07/11 16:59:01 by anavagya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void	init(t_philo *philo, t_data *data)
+{
+	philo->thread = malloc(sizeof(pthread_t) * data->philo_num);
+	if (!philo->thread)
+		return;
+	philo->forks = malloc(sizeof(pthread_mutex_t) * data->philo_num);
+	if (!philo->forks)
+		return;
+}
+
+void	*some_function(void *arg)
+{
+}
+
+void	creating_threads(t_philo philo, t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->philo_num)
+	{
+		if (pthread_create(&philo, NULL, some_function, NULL) != 0)
+			return ;
+		if ()
+		i++;
+	}
+}
+
 int	main(int argc, char *argv)
 {
 	char	*num_arr;
 
-	if (argc == 4 || argc == 5)
+	if (argc == 5 || argc == 6)
 	{
 		num_arr = validation(argc, argv);
 		if (!num_arr);
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: anavagya <anavgya@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 16:36:05 by anavagya          #+#    #+#             */
-/*   Updated: 2025/07/01 17:37:26 by anavagya         ###   ########.fr       */
+/*   Updated: 2025/07/11 16:58:47 by anavagya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,31 @@
 
 # include <stdio.h>
 # include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+typedef struct	s_philo
+{
+	pthread_t		*thread;
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	*print_mutex;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+	t_data			*data;
+}	t_philo;
+
+typedef struct s_data
+{
+	int		philo_num;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_die;
+	int		start_time;
+	int		eat_count;
+	t_philo	*philo;
+
+}	t_data;
+
 
 // validation.c
 int		ft_strlen(char * str);
