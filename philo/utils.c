11328be6263eac965432f6b8491dd3da74de94a0
Ani Avagyan <anavagya@c2r11s1.42yerevan.am>
2025-07-17 16:35:31 +0400

    added print_status function

--- philo/utils.c
+++ philo/utils.c
@@ -6,11 +6,22 @@
 /*   By: anavagya <anavgya@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 16:19:09 by anavagya          #+#    #+#             */
-/*   Updated: 2025/07/15 15:58:17 by anavagya         ###   ########.fr       */
+/*   Updated: 2025/07/17 16:33:27 by anavagya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	print_status(t_data *data, char *msg)
+{
+	long	timestamp;
+
+	pthread_mutex_lock(&data->print_mutex);
+	timestamp = get_time_in_ms() - data->start_time;
+	if (!data->philo_is_dead)
+	printf("%ld %d %s\n", timestamp, data->philo->id, msg);
+	pthread_mutex_unlock(&data->print_mutex);
+}
 
 long	get_time_in_ms(void)
 {
